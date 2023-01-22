df2498fd2d73bbd322455b21c7a3803d853e97f0
kylearmour1 <kylejarmour@gmail.com>
2023-01-22 00:26:15 -0800

    saving

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var pages = document.querySelectorAll(".page");
+  var currentPage = 0; 
+  var timeLeft = 100000;
+  var timerInterval;
+  var score = 0;
+
+  pages[currentPage].style.display = "block";
+//creating start button and making sure page is hidden when clicked//
+  var startButton = document.createElement("button");
+  startButton.innerHTML = "Start";
+  startButton.classList.add("start-button");
+  startButton.addEventListener("click", function(){
+    pages[currentPage].style.display = "none";
+    currentPage++;
+    pages[currentPage].style.display = "block";
+    //start the timer//
+    timerInterval = setInterval(function(){
+      timeLeft--;
+      document.querySelector("#time-left").innerHTML = timeLeft;
+      if(timeLeft <= 0){
+        pages[currentPage].style.display = "none";
+        clearInterval(timerInterval);
+        currentPage = 6;
+        pages[currentPage].style.display = "block";
+      alert("Times Up!");
+      }
+      
+    }, 1000);
+
+  });
+  
+  document.querySelector(".page").appendChild(startButton);
+
+  for (var i = 1; i < pages.length; i++) {
+    
+    var answerButtons = pages[i].querySelectorAll(".answer-button");
+    for(var j = 0; j < answerButtons.length; j++) {
+      answerButtons[j].addEventListener("click", function() {
+        if (this.id ==="correct"){
+          score++
+        }
+
+        pages[currentPage].style.display = "none";
+        currentPage++;
+        pages[currentPage].style.display = "block";
+      });
+      
+    }
+        
+    pages[i].style.display = "none";
+    
+    if(currentPage === 6) {
+      var finalScore = document.querySelector("#finalScore");
+        finalScore.innerHTML = "Your final score is: " +score;
+      
+      
+     
+    }
+
+  }});
+
